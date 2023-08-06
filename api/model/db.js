5dec19f53c7d5d2f29319312a3415121889144bc
Julian Silva <31382024+juliansempre@users.noreply.github.com>
2023-08-05 21:15:55 -0300

    Update db.js

--- api/model/db.js
+++ api/model/db.js
@@ -1,9 +1,18 @@
 import mysql from 'mysql';
 
 export const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "crud"
+    host: "85.10.205.173",
+    user: "rootportfolio1",
+    password: "toor123@",
+    port: 3306,
+    database: "bancojulian1"
 });
+
+//Local
+// export const db = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "",
+//     database: "crud"
+// });
    
